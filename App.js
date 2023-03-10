dd9348ef453a8f17585346fd40c8dada48839551
James Mokua <jamieedgar02@gmail.com>
2023-03-11 00:54:59 +0300

    Navigation and Bottom Tabs added

--- App.js
+++ App.js
@@ -1,61 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { Button} from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
-import {SafeAreaView,SafeAreaProvider } from 'react-native-safe-area-context'
-import * as SplashScreen from 'expo-splash-screen'
-import { useState,useEffect,useCallback} from 'react';
-import * as Font from 'expo-font';
+import { StatusBar } from "expo-status-bar";
+import { Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
+import * as SplashScreen from "expo-splash-screen";
+import { useState, useEffect, useCallback } from "react";
+import * as Font from "expo-font";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-SplashScreen.preventAutoHideAsync();//prevents splashscreen from going away
+import ChatSettingsScreen from "./screens/ChatSettingsScreen";
+import SettingsScreen from "./screens/SettingsScreen";
+import ChatListScreen from "./screens/ChatListScreen";
 
+SplashScreen.preventAutoHideAsync(); //prevents splashscreen from going away
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 export default function App() {
-  const[appIsLoaded,setAppIsLoaded] = useState(false)
+  const [appIsLoaded, setAppIsLoaded] = useState(false);
 
-useEffect(() => {
-  // Load fonts
-  async function loadFonts() {
-    try{
-    await Font.loadAsync({
-      "black": require("./assets/fonts//Roboto-Black.ttf"),
-          "blackItalic": require("./assets/fonts/Roboto-BlackItalic.ttf"),
-          "bold": require("./assets/fonts/Roboto-Bold.ttf"),
-          "boldItalic": require("./assets/fonts/Roboto-BoldItalic.ttf"),
-          "italic": require("./assets/fonts/Roboto-Italic.ttf"),
-          "light": require("./assets/fonts/Roboto-Light.ttf"),
-          "lightItalic": require("./assets/fonts/Roboto-LightItalic.ttf"),
-          "medium": require("./assets/fonts/Roboto-Medium.ttf"),
-          "mediumItalic": require("./assets/fonts/Roboto-MediumItalic.ttf"),
-          "regular": require("./assets/fonts/Roboto-Regular.ttf"),
-          "thin": require("./assets/fonts/Roboto-Thin.ttf"),
-          "thinItalic": require("./assets/fonts/Roboto-ThinItalic.ttf"),
-    })} catch(error){
-      console.log(error);
-    } finally{
-      setAppIsLoaded(true);
-    };
+  useEffect(() => {
+    // Load fonts
+    async function loadFonts() {
+      try {
+        await Font.loadAsync({
+          black: require("./assets/fonts//Roboto-Black.ttf"),
+          blackItalic: require("./assets/fonts/Roboto-BlackItalic.ttf"),
+          bold: require("./assets/fonts/Roboto-Bold.ttf"),
+          boldItalic: require("./assets/fonts/Roboto-BoldItalic.ttf"),
+          italic: require("./assets/fonts/Roboto-Italic.ttf"),
+          light: require("./assets/fonts/Roboto-Light.ttf"),
+          lightItalic: require("./assets/fonts/Roboto-LightItalic.ttf"),
+          medium: require("./assets/fonts/Roboto-Medium.ttf"),
+          mediumItalic: require("./assets/fonts/Roboto-MediumItalic.ttf"),
+          regular: require("./assets/fonts/Roboto-Regular.ttf"),
+          thin: require("./assets/fonts/Roboto-Thin.ttf"),
+          thinItalic: require("./assets/fonts/Roboto-ThinItalic.ttf"),
+        });
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setAppIsLoaded(true);
+      }
+    }
+    loadFonts();
+  }, []);
+
+  const onLayout = useCallback(async () => {
+    if (appIsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsLoaded]);
+
+  if (!appIsLoaded) {
+    return null;
   }
-  loadFonts()
-},[]);
-
-const onLayout = useCallback(async () => {
-  if (appIsLoaded) {
-    await SplashScreen.hideAsync();
-  }
-}, [appIsLoaded]);
-
-if (!appIsLoaded) {
-  return null;
+const TabNavigator = () =>{
+  return(
+    <Tab.Navigator screenOptions={{headerTitle: ''}}>
+    <Tab.Screen name="ChatList" component={ChatListScreen} options={{tabBarLabel: 'Chats'}}/>
+    <Tab.Screen name="Settings" component={SettingsScreen} options={{
+      tabBarLabel: 'Settings'
+    }}/>
+  </Tab.Navigator>
+  )
 }
-  function clickHandler(){
-    alert('Hi');
-  }
   return (
     <SafeAreaProvider style={styles.container} onLayout={onLayout}>
-      <SafeAreaView >
-      <Text style={styles.label}>Hello There!</Text>
-      <StatusBar style="auto" />
-      <Button title='Click me' onPress={clickHandler}/>
-      </SafeAreaView>
+     
+        <NavigationContainer>
+          <StatusBar style="auto" />
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={TabNavigator} options={{headerShown: false}}/>
+            <Stack.Screen name="ChatSettings" component={ChatSettingsScreen} options={{
+              headerTitle: 'Chat Settings',
+              headerBackTitle: 'Back'
+            }}/>
+          </Stack.Navigator>
+        </NavigationContainer>
+     
     </SafeAreaProvider>
   );
 }
@@ -63,12 +86,10 @@ if (!appIsLoaded) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
-  label:{
+  label: {
     fontSize: 20,
-    fontFamily: "regular"
-  }
+    fontFamily: "regular",
+  },
 });
